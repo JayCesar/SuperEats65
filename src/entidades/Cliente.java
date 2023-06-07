bdb5b53d1eaf1db53956d9dca00c7e1609e3e603
JayCesar <batistajulio215@gmail.com>
2023-06-07 20:56:24 -0300

    atualiza a classe pessoa

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -1,6 +1,58 @@
 package entidades;
 
-public class Cliente {
-	// Falta Herdar da clsse pessoa
-	// Adicionar somente os seguintes atributos: cpf, telefone, dataCadastro
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Cliente extends Pessoa{
+	private String cpf;
+	private Integer telefone;
+	private LocalDate dataCadastro;
+	
+	public Cliente(Integer id, String nome, String email, LocalDate dataDeNascimento, String senha, String cpf,
+			Integer telefone, LocalDate dataCadastro) {
+		super(id, nome, email, dataDeNascimento, senha);
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public boolean efetuarPagamento(double totalPedido) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Total a pagar: " + String.format("%.2f", totalPedido));
+		System.out.println("Digite o valor total a pagar: ");
+		double pagamento = sc.nextDouble();
+		if (pagamento >= totalPedido) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	
+	
 }
